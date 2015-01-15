c27251ef9a5b681f2a1f66e9c0fb1f5ca6b63f61
Daniel <daniel.bagge@gmail.com>
2015-01-15 12:07:18 +0100

    Initial commit

---
+++ app/src/main/java/se/mah/bagge/accelerometer/Main.java
@@ -0,0 +1,27 @@
+package se.mah.bagge.accelerometer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class Main extends Activity {
+    SensorService sservice = new SensorService();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast toast = Toast.makeText(this, "shake counter: " + sservice.getCounter(), Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+        toast.show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/se/mah/bagge/accelerometer/SensorService.java
@@ -0,0 +1,82 @@
+package se.mah.bagge.accelerometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+
+public class SensorService extends Service implements SensorEventListener {
+    private static final String DEBUG_TAG = "SensorService";
+    private long lastUpdate;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 500;
+    private int counter = 0;
+    private SensorManager sensorManager = null;
+    private Sensor sensor = null;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor , SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // do nothing
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            long curTime = System.currentTimeMillis();
+            // only allow one update every 100ms.
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x+y+z - last_x - last_y - last_z)/ diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    counter++;
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+//        new SensorEventLoggerTask().execute(event);
+        sensorManager.unregisterListener(this);
+        stopSelf();
+    }
+
+//    private class SensorEventLoggerTask extends AsyncTask<SensorEvent, Void, Void> {
+//        @Override
+//        protected Void doInBackground(SensorEvent... events) {
+//            SensorEvent event = events[0];
+//        }
+//    }
+}
